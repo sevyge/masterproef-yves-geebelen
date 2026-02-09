323b66fca935f7f21ddfd6e791e896e4156626b6
sevyge <sevy.geebelen@gmail.com>
2026-02-09 19:58:31 +0100

    Update serverStatus.js

--- serverStatus.js
+++ serverStatus.js
@@ -1,23 +1,32 @@
 'use strict';
 
 const serverStatusElement = document.getElementById('serverStatus');
+let lastStatus = 'unknown';
 
 function setServerStatus(text, color) {
     serverStatusElement.innerHTML = `<span style="color:${color}">● </span>${text}`;
 }
 
 async function checkServerStatus() {
-    setServerStatus('Connecting to server...', 'orange');
+    if (lastStatus !== "online") {
+        setServerStatus('Connecting to server...', 'orange');
+    }
     try {
         const response = await fetch("http://localhost:8000" + '/');
         if (response.ok) {
+            lastStatus = "online";
             setServerStatus('Server status: Online', 'green');
         } else {
+            lastStatus = "error";
             setServerStatus('Server status: Error', 'red');
         }
     } catch (error) {
+        lastStatus = "offline";
         setServerStatus('Server status: Offline', 'red');
     }
 }
 
-window.addEventListener('load', checkServerStatus);
+window.addEventListener('load', () => {
+    checkServerStatus();
+    setInterval(checkServerStatus, 10000);
+});
