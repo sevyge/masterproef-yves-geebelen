0057bd5aa256347d58ec0781947738bed1a0b155
sevyge <138933118+sevyge@users.noreply.github.com>
2026-06-01 02:06:33 +0200

    Fix CORS issue

--- backend/services/storage_service.py
+++ backend/services/storage_service.py
@@ -11,6 +11,7 @@ import io
 from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 load_dotenv(os.path.join(BASE_DIR, "..", ".env"))
 
 # Google Drive setup
